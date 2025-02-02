07783d8fe29b2229140a048f9dc65f7aabbf3044
Erick Aguilar <auge_1985@hotmail.com>
2025-02-02 04:30:26 -0600

    Actualiza dependencias de Angular a la versión 16 y simplifica el guard de autenticación

--- src/app/modules/auth/pages/guard/auth.guard.ts
+++ src/app/modules/auth/pages/guard/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private router: Router) { }
 
